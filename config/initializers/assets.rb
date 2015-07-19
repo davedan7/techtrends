bbe1c9c56b41f714052a24f26efc090b8d5d5d53
David Daniel <davedan7@gmail.com>
2015-07-19 13:42:56 -0600

    Changed assets. Can't get pictures on heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( react_init.js )
+Rails.application.config.assets.precompile += %w( init.js )
